b1f32443eaf182bab14713ac121995f8ad0fadd2
sudogem <sudogem@gmail.com>
2022-05-31 14:29:02 -0500

    1.0.0
     Author:    sudogem <sudogem@gmail.com>
     Date:      Tue May 31 14:24:43 2022 -0500

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module SkillsoftRorExercise
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    VERSION = '1.0.0'
   end
 end
